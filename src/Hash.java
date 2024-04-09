77c90146e2e0184cf776331e0597467dd7fedce0
alya1b <93484747+alya1b@users.noreply.github.com>
2024-04-09 12:04:32 +0300

    Update Hash.java

--- src/Hash.java
+++ src/Hash.java
@@ -10,12 +10,30 @@ import java.util.Random;
 
 public class Hash implements AM {
     private static long startTime = 0;
+    private static final BigInteger MODULE = new BigInteger("2147483647");
+    private static final BigInteger BASE =  new BigInteger("31");
+	 
+	public static BigInteger computeHash(String str) {
+		 BigInteger hashValue = BigInteger.ZERO;
+	     BigInteger powBase = BigInteger.ONE;
+
+	     for (int i = 0; i < str.length(); i++) {
+	    	 char ch = str.charAt(i);
+	         BigInteger charValue = BigInteger.valueOf(ch - 'a' + 1);
+	         hashValue = (hashValue.add(charValue.multiply(powBase).mod(MODULE))).mod(MODULE);
+	         powBase = powBase.multiply(BASE).mod(MODULE);
+	        }
+	        return hashValue;
+	 }
+
 
     public static void main(String[] args) throws Exception {
+    	
+    	
         System.err.println("Preparing...");
-
+        
         if (args.length != 1) {
-            System.err.println("Number of workers not specified");
+            System.err.println("Nnumber of workers not specified");
             System.exit(1);
         }
 
@@ -26,98 +44,79 @@ public class Hash implements AM {
         AMInfo info = new AMInfo(curtask, null);
 
         System.err.println("Reading input...");
-
-        String inputFileName = "input.txt"; // Input file name
-        String input = "";
-        try {
-            Scanner scanner = new Scanner(new File(inputFileName));
-            input = scanner.nextLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
-        String[] inputs = input.split(" ");
-        int N = Integer.parseInt(inputs[0]);
-        double x1 = Double.parseDouble(inputs[1]);
-        double x2 = Double.parseDouble(inputs[2]);
-        double y1 = Double.parseDouble(inputs[3]);
-        double y2 = Double.parseDouble(inputs[4]);
-
-        System.err.println("N: " + N);
-        System.err.println("x1: " + x1);
-        System.err.println("x2: " + x2);
-        System.err.println("y1: " + y1);
-        System.err.println("y2: " + y2);
+       
+        
+	String S = "";
+	try{
+		Scanner sc = new Scanner(new File(info.curtask.findFile("input.txt")));
+		S = sc.nextLine();
+	}
+	catch (IOException e) {e.printStackTrace(); return;}
+        
+        int len = S.length();
+        int sub_len = (len + n - 1) / n;
 
         System.err.println("Forwarding parts to workers...");
-        startTime = System.nanoTime();
+       startTime = System.nanoTime();
         channel[] channels = new channel[n];
-        int remainder = N % n;
-        int chunkSize = N / n;
         for (int i = 0; i < n; i++) {
-            int size = chunkSize + ((i < remainder) ? 1 : 0);
+            String substring = "";
+	    if (i * sub_len < S.length()) {
+		substring = S.substring(i * sub_len, 
+            		Math.min((i * sub_len + sub_len), S.length()));
+		}
             point p = info.createPoint();
             channel c = p.createChannel();
             p.execute("Hash");
-            ArrayList<Double> params = new ArrayList<>();
-            params.add((double) size);
-            params.add(x1);
-            params.add(x2);
-            params.add(y1);
-            params.add(y2);
-            c.write(params);
+            c.write(substring);
             channels[i] = c;
         }
 
         System.err.println("Getting results");
-
-        int result = 0;
+      
+        BigInteger[] sub_hash = new BigInteger[n];
         for (int i = 0; i < n; i++) {
-            result += channels[i].readInt();
+        	sub_hash[i] = (BigInteger) channels[i].readObject();
         }
 
         System.err.println("Calculation of the result");
-
-        double integral = (double) result * (x2 - x1) * (y2 - y1) / N;
-
-        long endTime = System.nanoTime();
-
-        System.out.println("Result: " + integral);
-
+     
+        BigInteger hash = resultСalculation(sub_hash, sub_len);
+       
+ 	long endTime = System.nanoTime();
+	
+        System.out.println("Result: " + hash.toString());
+       
+        
         long timeElapsed = endTime - startTime;
         double seconds = timeElapsed / 1_000_000_000.0;
         System.err.println("Time passed: " + seconds + " seconds.");
-
+        
+        
         curtask.end();
     }
 
+
     public void run(AMInfo info) {
-        System.err.println("Getting part from parent...");
-        ArrayList<Double> params = (ArrayList<Double>) info.parent.readObject();
-        int N1 = params.get(0).intValue();
-        double x1 = params.get(1);
-        double x2 = params.get(2);
-        double y1 = params.get(3);
-        double y2 = params.get(4);
+     
+        String substring = (String)info.parent.readObject();
+        BigInteger subhash = computeHash(substring);
 
-        Random random = new Random();
-        int num = 0;
-        for (int i = 0; i < N1; i++) {
-            double x = random.nextDouble() * (x2 - x1) + x1;
-            double y = random.nextDouble() * (y2 - y1) + y1;
-            double fun = foo(x);
-            if (0 <= y && y <= fun) {
-                num += 1;
-            } else if (0 >= y && y >= fun) {
-                num -= 1;
-            }
-        }
-
-        info.parent.write(num);
+        info.parent.write(subhash);
+  
     }
 
-    public static double foo(double x) {
-        return 1 / (Math.pow(x, 5) + 1);
+    public static BigInteger resultСalculation(BigInteger[] subhash, int sublen) {
+        
+        BigInteger delt = BigInteger.ONE;
+        BigInteger step = BASE.modPow(BigInteger.valueOf(sublen), MODULE);
+        BigInteger output = BigInteger.ZERO;
+        
+        for (BigInteger x : subhash) {
+	     System.out.println(x.toString());
+            output = output.add(x.multiply(delt).mod(MODULE)).mod(MODULE);
+            delt = delt.multiply(step).mod(MODULE);
+        }
+       return output;
     }
 }
